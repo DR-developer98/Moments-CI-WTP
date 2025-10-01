afd136204c3a87a7330a9ae5911b2baf297e7b7f
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-10-01 18:00:11 +0200

    Define response and request interceptors and incorporate them in CurrentUserContext.js

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -6,3 +6,7 @@ axios.defaults.baseURL = "https://drf-api-wtp-project-731b35348821.herokuapp.com
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // ↓ voorkomt foutmeldingen wanneer we cookies versturen ↓
 axios.defaults.withCredentials = true
+
+// 4. INTERCEPTORS; axiosReq = request interceptor, axiosRes = response interceptor
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,32 +1,84 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
+import { useHistory } from "react-router";
+
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
 
-// 3h. Om de Context-objecten en createContext niet iedere keer te hoeven importeren
-// maken we hier twee custom-hooks aan
-export const useCurrentUser = () => useContext(CurrentUserContext)
-export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
+//3i. Deze custom hooks hebben we aangemaakt, zodat we niet altijd de useContext
+// en de Context-objecten hoeven te importeren.
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
   const handleMount = async () => {
     try {
-      const { data } = await axios.get("dj-rest-auth/user/");
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
     }
   };
+  
   useEffect(() => {
     handleMount();
   }, []);
-
-   return (
-        <CurrentUserContext.Provider value={currentUser}>
-            <SetCurrentUserContext.Provider value={setCurrentUser}>
-                {children}
-            </SetCurrentUserContext.Provider>
-        </CurrentUserContext.Provider>
+  
+// INTERCEPTORS --> gewoon kopiëren en bron vermelden Moments CI WTP
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history.push("/signin");
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
     );
+
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  }, [history]);
+  // ↑↑↑ [history] --> we willen dat useMemo alleen één keer runt
+  // we zouden een foutmelding krijgen, als we een lege array zouden gebruiken.
+  // vanwaar we [history] gebruiken.
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
 };
